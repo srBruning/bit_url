aa07d3fb17e8c95651a4808ccf60ba5251b81033
Diego Bruning <diego.brg92@gmail.com>
2021-04-23 11:22:37 -0300

    pwa

---
+++ core/src/public/precache-manifest.7bf2f25a8750daa0338d301d1d31e023.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "d457726bfe904756d5f1",
+    "url": "/home/static/js/app.715f6d92.chunk.js"
+  },
+  {
+    "revision": "d40231c033c19130d1e5",
+    "url": "/home/static/js/runtime~app.8bf13c6e.js"
+  },
+  {
+    "revision": "75848e818c5df9546e36",
+    "url": "/home/static/js/2.a59dd38c.chunk.js"
+  },
+  {
+    "revision": "9d2f9c246d14cb6a420e02eeede99be7",
+    "url": "/home/static/media/4br.fb9c7251.png"
+  },
+  {
+    "revision": "e5c4ba08c1a82ef1dc0c72885b61ca05",
+    "url": "/home/static/media/demo.fbea4d7d.svg"
+  },
+  {
+    "revision": "55255111eb76cece5a9bc395254a1c89",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "b8dbc65877ab61b8522020f62ab000fd",
+    "url": "/home/manifest.json"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/home/serve.json"
+  },
+  {
+    "revision": "2002987acac9deb6803ab4ba381226ba",
+    "url": "/home/expo-service-worker.js"
+  },
+  {
+    "revision": "197b51a3fee1fc6ae429787caacff3c6",
+    "url": "/home/register-service-worker.js"
+  },
+  {
+    "revision": "1f56b5d66f4e33a43098ac5bd1d1346c",
+    "url": "/home/static/js/2.a59dd38c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d324ddc823eca6c86688f62576c81e76",
+    "url": "/home/favicon-16.png"
+  },
+  {
+    "revision": "e72daee58e37c99d3553059eda418aa8",
+    "url": "/home/favicon-32.png"
+  },
+  {
+    "revision": "a7acda9210941f0252157c7a374ddaa2",
+    "url": "/home/favicon.ico"
+  }
+];
